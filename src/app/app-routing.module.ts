8fd8a95e1ee5189e422051e8cac3cfa1477cda5d
SrtaK <atorrecillam@gmail.com>
2021-05-23 14:37:31 +0200

    landing apanada, falta maquetar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/components/contacto/contacto.component';
 import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 import { ErrorComponent } from './shared/error/error.component';
 
 
@@ -24,6 +25,11 @@ const routes: Routes = [
       },{
         path: 'contacto',
         component: ContactoComponent
+      },
+      {
+        path: '',
+        component: LandingComponent
+
       }
 
     ]
