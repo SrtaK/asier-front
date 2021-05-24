24bb7fd9e910de2c22fd888998ddf8f90ffded70
SrtaK <atorrecillam@gmail.com>
2021-05-24 17:03:18 +0200

    baseUrl corregida

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //insertar aquí la base url de producción
- baseUrl: 'https://prueba-asier-biota.herokuapp.com/api/'
+ baseUrl: 'https://prueba-asier-biota.herokuapp.com/api'
 
 
 };
